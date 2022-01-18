24a8cad5c3c5987a703634b90c5fea3ba45ab52d
Malika Satimbaeva <malika@360fin.tech>
2022-01-18 22:00:37 +0600

    Role field fix

--- authe/managers.py
+++ authe/managers.py
@@ -23,9 +23,9 @@ class CustomUserManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('role', 'A')
+        extra_fields.setdefault('role', 1)
 
-        if extra_fields.get('role') != 'A':
+        if extra_fields.get('role') != 1:
             raise ValueError('Superuser must have role of Global Admin')
         user = self.create_user(email, password, **extra_fields)
 
