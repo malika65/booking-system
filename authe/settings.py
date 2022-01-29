fae7365c2b0728e671e41630a57872ed5e94dbf5
Malika Satimbaeva <malika@360fin.tech>
2022-01-29 21:40:52 +0600

    Fix outstanding token

--- authe/settings.py
+++ authe/settings.py
@@ -5,7 +5,7 @@ from django.test.signals import setting_changed
 from django.utils.translation import gettext_lazy as _
 from rest_framework.settings import APISettings as _APISettings
 
-from .utils import format_lazy
+from .token_utils import format_lazy
 
 USER_SETTINGS = getattr(settings, 'SIMPLE_JWT', None)
 
