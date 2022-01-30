123c9bdf8859128a1007faf868124576fd004fe3
Malika Satimbaeva <malika@360fin.tech>
2022-01-30 14:03:57 +0600

    Fix outstanding token in admin model

--- authe/admin.py
+++ authe/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 from .models import User
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 
 class PersonAdmin(admin.ModelAdmin):
     model = User
-    def has_delete_permission(self, request, obj=None):
-        return True
+    
+    def BE_AWARE_NO_WARNING_clear_tokens_and_delete(self, request, queryset):
+        users = queryset.values("id")
+        OutstandingToken.objects.filter(user__id__in=users).delete()
+        queryset.delete()
+
+    actions = ["BE_AWARE_NO_WARNING_clear_tokens_and_delete"]
 
 admin.site.register(User)
