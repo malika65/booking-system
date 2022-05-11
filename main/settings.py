e9b8cbb349636abce1f7579f918a69af33794d37
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-05-11 16:15:06 +0600

    Allowed orogin

--- main/settings.py
+++ main/settings.py
@@ -44,7 +44,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = ['https://silk-travel.herokuapp.com']
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:3000/',
