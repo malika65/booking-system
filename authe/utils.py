2237167242d701eef41079c451b3180e63d94c4a
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-05-31 12:07:57 +0600

    Tasks fix

--- authe/utils.py
+++ authe/utils.py
@@ -9,17 +9,6 @@ from authe.models import User
 from main.celery import app
 
 
-class EmailThread(threading.Thread):
-
-    def __init__(self, email):
-        self.email = email
-        threading.Thread.__init__(self)
-
-    @app.task
-    def run(self):
-        self.email.send()
-
-
 @app.task
 def send_email(user, current_site):
     token = RefreshToken.for_user(user).access_token
@@ -31,7 +20,7 @@ def send_email(user, current_site):
                 'email_subject': 'Verify your email'}
     email = EmailMessage(
         subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-    EmailThread(email).start()
+    email.send()
 
 
 @app.task
@@ -42,11 +31,11 @@ def send_code_to_email(user_id, code):
                 'email_subject': 'Ваш код для подтверждения почты'}
     email = EmailMessage(
         subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-    EmailThread(email).start()
+    email.send()
 
 
 @app.task
 def send_reset_email(data):
     email = EmailMessage(
         subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-    EmailThread(email).start()
+    email.send()
