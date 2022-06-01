0252e64d9fe877f1c49646ed0a798acf31e8b0ef
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-06-02 00:18:06 +0600

    Add backup shedule

---
+++ backup_maker.py
@@ -0,0 +1,28 @@
+import datetime
+
+import schedule
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import subprocess
+
+
+def make_backup():
+    day_of_month = datetime.datetime.now().day
+    if (7 < day_of_month < 15) or day_of_month > 21:
+        gauth = GoogleAuth()
+        drive = GoogleDrive(gauth)
+
+        subprocess.run(["heroku", "pg:backups:download"])
+
+        upload_file_list = ['latest.dump']
+        for upload_file in upload_file_list:
+            gfile = drive.CreateFile({'title': upload_file, 'parents': [{'id': '1R10r7X_zmdsokfbIwZk8MxG5ces46mR2'}]})
+            gfile.SetContentFile(upload_file)
+            gfile.Upload()
+        subprocess.run(["rm", "-rf", "latest.dump"])
+
+
+schedule.every().friday.do(make_backup())
+
+# Restore command
+# subprocess.run(["heroku", "pg:backups:restore", 'https://drive.google.com/uc?id=1UCwErrCtrOkBRmKEXTt6i7Qg40LxJ8z1&export=download', "DATABASE_URL", "--confirm", "silk-travel"])
--- main/celery.py
+++ main/celery.py
@@ -1,26 +1,17 @@
-import os
 import logging
+import os
 import ssl
 
 from celery import Celery
 from celery.signals import after_setup_logger
-
-from django.conf import settings
 from django.core import management
-# this code copied from manage.py
-# set the default Django settings module for the 'celery' app.
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-# you can change the name here
-# app = Celery("main")
 app = Celery("main")
 
-# print(f'connected to celery using broker: {settings.CELERY_BROKER_URL} and backend: {settings.CELERY_RESULT_BACKEND}')
-# read config from Django settings, the CELERY namespace would make celery
-# config keys has `CELERY` prefix
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# discover and load tasks.py in django apps
 app.autodiscover_tasks()
 #
 app.conf.update(BROKER_URL='rediss://:p2c97d946f1f1a0246db67f32f70ec89435736f51dffdcf9f7db96dcdcb16ca3c@ec2-3-251-43-118.eu-west-1.compute.amazonaws.com:25650',
