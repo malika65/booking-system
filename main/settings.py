c914dcd6a6528a55ef99d8b7ee14bf89bfbe5c8d
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-06-07 11:50:07 +0600

    Db

--- main/settings.py
+++ main/settings.py
@@ -299,7 +299,7 @@ DATABASES = {
 }
 
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(default=os.environ['DATABASE_URL'])
 DATABASES['default'].update(db_from_env)
 
 
