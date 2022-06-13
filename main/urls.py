cc49d2f56defb8be22c25e35a37f2dd4d396b84b
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-06-13 17:12:31 +0600

    Fix images

--- main/urls.py
+++ main/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
 )
 
 urlpatterns += doc_urls
-
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
