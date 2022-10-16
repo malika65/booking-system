94c43c49579cb6f171a2567e141f779520a1035e
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-10-16 23:01:08 +0600

    Fix settings

--- main/settings.py
+++ main/settings.py
@@ -172,12 +172,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework_api_key.permissions.HasAPIKey',
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 25
