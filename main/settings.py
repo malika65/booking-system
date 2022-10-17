658b96b5bc97feb7876d6f521514d0706fa705eb
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-10-17 15:11:12 +0600

    Add cors

--- main/settings.py
+++ main/settings.py
@@ -40,11 +40,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-
-# CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = ['https://silk-travel.herokuapp.com',
                         'https://silkwaytravel.kg']
