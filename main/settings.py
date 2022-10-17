18bf2ba7a7c825caa2ec236c3c69166f1b2f8d83
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-10-17 13:10:46 +0600

    Fix settings

--- main/settings.py
+++ main/settings.py
@@ -343,7 +343,7 @@ CACHES = {
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'CONNECTION_POOL_KWARGS': {'ssl_cert_reqs': None,
-                                       "ssl": True
+                                       # "ssl": True
                                        },
         }
     }
